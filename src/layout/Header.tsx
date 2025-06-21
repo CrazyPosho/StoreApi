f799b2d6256d7dde3147003a95c46de7954020b1
David Hernandez <davidjorgeagramonte@gmail.com>
2025-06-21 00:49:07 -0400

    refactor: actualizar el doctype del HTML, mejorar los estilos del encabezado y agregar animación de gradiente
    Se cambió la declaración del doctype en index.html para mantener la consistencia.
    
    Se actualizó package.json para incluir nuevas dependencias de linting y del compilador de React.
    
    Se mejoró index.css agregando una animación de gradiente para mejorar los efectos visuales.
    
    Se modificó el componente Header para usar un fondo con gradiente en el nombre de la tienda y se mejoró el diseño.
    
    Se actualizó la configuración de Vite para integrar MillionLint y el plugin del compilador de React.

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -3,14 +3,21 @@ import { useChangeMode } from "../hooks/ChangeMode";
 import { ChangeModeButton } from "../components/ChangeModeButton";
 import { Link } from "wouter";
 
-
 export const Header = () => {
   const { toggleTheme, theme } = useChangeMode();
   return (
     <>
-      <div className="dark:bg-black/90 dark:text-white bg-white/90 text-black p-2 font-extralight  w-full  relative flex justify-between text-xl border-b-1 border-black">
-        <div>PoshitoStore</div>
-        <div className="flex gap-8  ">
+      <div className="dark:bg-black/90 dark:text-white bg-white/90 text-black p-2 font-extralight  w-full  relative flex justify-between items-center text-xl border-b-1 border-black ">
+        <Link
+          href="/"
+          className="cursor-pointer
+      bg-gradient-to-r from-black via-gray-200 to-black
+      text-transparent bg-clip-text
+      bg-[length:200%_auto] animate-gradient-pan"
+        >
+          PoshitoStore
+        </Link>
+        <div className="flex gap-8">
           <Link href="/product" className="hover:scale-125 transition-all">
             Shop
           </Link>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
+import MillionLint from "@million/lint";
+// vite.config.ts
+
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react"; 
 import tailwindcss from "@tailwindcss/vite";
+
+const ReactCompilerConfig = {};
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [MillionLint.vite({
+    enabled: true
+  }), react({
+    babel: {
+      plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+    },
+  }), tailwindcss()],
 });
